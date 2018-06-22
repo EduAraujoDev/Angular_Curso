b45ced28f43d6161ce6cc41f72c3a52a9a3269ce
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-06-21 23:04:50 -0300

    app2 - recuperando as ofertas do tipo restaurante pela API fake

--- app2/src/app/restaurantes/restaurantes.component.ts
+++ app2/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
-  selector: 'app-restaurantes',
-  templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+    selector: 'app-restaurantes',
+    templateUrl: './restaurantes.component.html',
+    styleUrls: ['./restaurantes.component.css'],
+    providers: [ OfertasService ]
 })
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+    public ofertas: Oferta[];
 
-  ngOnInit() {
-  }
+    constructor(private ofertasServices: OfertasService) { }
 
+    ngOnInit() {
+        this.ofertasServices.getOfertasPorCategoria('restaurante')
+            .then((ofertas: Oferta[]) => {
+                this.ofertas = ofertas;
+            })
+    }
 }
