bcaa12bb726a0ba4b4421498ec66e18d169ece7d
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-06-21 22:08:28 -0300

    app2 - criado o mapa de rotas

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -10,19 +13,21 @@ import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopoComponent,
-    HomeComponent,
-    RodapeComponent,
-    RestaurantesComponent,
-    DiversaoComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopoComponent,
+        HomeComponent,
+        RodapeComponent,
+        RestaurantesComponent,
+        DiversaoComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        RouterModule,
+        RouterModule.forRoot(ROUTES)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
