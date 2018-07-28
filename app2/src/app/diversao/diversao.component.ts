9b78f4d239e8f5bfcf8d8c3b78d89ec6105dd3da
Eduardo Filho <efilho@SHOEBIZ.LOCAL>
2018-07-28 19:59:29 -0300

    Revert "app2 - recuperando as ofertas do tipo diversão pela API fake"
    This reverts commit 962f5912244ea3c90742feb9f3a5cfc16317f377.

--- app2/src/app/diversao/diversao.component.ts
+++ app2/src/app/diversao/diversao.component.ts
@@ -1,24 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { OfertasService } from '../ofertas.service';
-import { Oferta } from '../shared/oferta.model';
 
 @Component({
-    selector: 'app-diversao',
-    templateUrl: './diversao.component.html',
-    styleUrls: ['./diversao.component.css'],
-    providers: [ OfertasService ]
+  selector: 'app-diversao',
+  templateUrl: './diversao.component.html',
+  styleUrls: ['./diversao.component.css']
 })
-
 export class DiversaoComponent implements OnInit {
 
-    public ofertas: Oferta[];
+  constructor() { }
 
-    constructor(private ofertasService: OfertasService) { }
+  ngOnInit() {
+  }
 
-    ngOnInit() {
-        this.ofertasService.getOfertasPorCategoria('diversao')
-            .then((ofertas: Oferta[]) => {
-                this.ofertas = ofertas;
-        })
-    }
 }
