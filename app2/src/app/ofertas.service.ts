a5d25ed1cbad5be6d98300956644fbd8df54ccf9
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-10-05 22:31:34 -0300

    app2 - Recuperando dados 'Onde fica?' via HTTP Request

--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -6,25 +6,33 @@ import { URL_API } from './app.api';
 @Injectable()
 export class OfertasService {
 
-    private url_api = 'http://localhost:3000/ofertas';
+    //private url_api = 'http://localhost:3000/ofertas';
 
     constructor(private http: Http) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get(`${URL_API}`)
+        return this.http.get(`${URL_API}/ofertas`)
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`${this.url_api}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json())
 	}
 	
 	public getOfertaPotId(id: number): Promise<Oferta> {
-		return this.http.get(`${this.url_api}?id=${id}`)
+		return this.http.get(`${URL_API}/ofertas?id=${id}`)
 			.toPromise()
 			.then((resposta: any) => resposta.json()[0])
-	}
+    }
+    
+    public getComoUsarOfertaporId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                return resposta.json()[0].descricao;
+            })
+    }
 }
