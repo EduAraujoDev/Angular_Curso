4b7fc33df2f9a86c8bb28808d4ab1cc9b3675aa1
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-11-03 12:52:01 -0300

    app2 - finalizando stream com error() e complete()

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -41,10 +41,14 @@ export class OfertaComponent implements OnInit {
         let meuObservableTeste = Observable.create((observer: Observer<number>) => {
             observer.next(1);
             observer.next(3);
+            //observer.error('Ocorreu um erro');
+            observer.complete();
         });
 
         meuObservableTeste.subscribe(
-            (resultado: number) => console.log(resultado + 10)
+            (resultado: number) => console.log(resultado + 10),
+            (erro: string) => console.log(erro),
+            () => console.log('Deu certo') 
         );
 	}
 }
