e550c4295f76fd60084b0ad43be3eb039cf59f67
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-11-03 12:44:53 -0300

    app2 - teste usando observable create

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
-import { Observable, interval } from 'rxjs';
+import { Observable, interval, Observer } from 'rxjs';
 
 @Component({
 	selector: 'app-oferta',
@@ -30,10 +30,21 @@ export class OfertaComponent implements OnInit {
         );
         */
         
+        /*
         let tempo = interval(2000);
 
         tempo.subscribe((intervalo: number) => {
             console.log(intervalo);
         })
+        */
+
+        let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+            observer.next(1);
+            observer.next(3);
+        });
+
+        meuObservableTeste.subscribe(
+            (resultado: number) => console.log(resultado + 10)
+        );
 	}
 }
