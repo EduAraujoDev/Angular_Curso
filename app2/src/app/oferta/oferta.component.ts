29de6aad9a087ca9f7b587380f41002077d3b439
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-01-22 20:02:54 -0200

    app2 - criado campo de pesquisa de oferta e usando eventbind para recuperar o que está sendo digitado no campo

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
-import { Observable, interval, Observer, Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-oferta',
@@ -13,9 +12,6 @@ import { Observable, interval, Observer, Subscription } from 'rxjs';
 
 export class OfertaComponent implements OnInit, OnDestroy {
 
-    private tempoObservableSubscription: Subscription;
-    private meuObservableTesteSubscription: Subscription;
-
 	private oferta: Oferta;
 
 	constructor(private route: ActivatedRoute, private ofertasServices: OfertasService) {}
@@ -25,44 +21,8 @@ export class OfertaComponent implements OnInit, OnDestroy {
 			.then(( oferta: Oferta) => {
 				this.oferta = oferta;
             });
-        /*  
-        this.route.params.subscribe(
-            (parametro: any) => { console.log(parametro) },
-            (erro: any) => console.log(erro),
-            () => console.log('Deu certo!')    
-        );
-        */
-        
-        /*
-        let tempo = interval(2000);
-
-        tempo.subscribe((intervalo: number) => {
-            console.log(intervalo);
-        })
-        */
-
-       let tempo = interval(2000);
-
-       this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
-           console.log(intervalo);
-       })
-
-        let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-            observer.next(1);
-            observer.next(3);
-            //observer.error('Ocorreu um erro');
-            observer.complete();
-        });
-
-        this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
-            (resultado: number) => console.log(resultado + 10),
-            (erro: string) => console.log(erro),
-            () => console.log('Deu certo') 
-        );
     }
     
     ngOnDestroy() {
-        this.meuObservableTesteSubscription.unsubscribe();
-        this.tempoObservableSubscription.unsubscribe();
     }
 }
