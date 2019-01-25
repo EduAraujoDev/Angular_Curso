01b8691a91dbd1db4576e4b4a0847bdff068d461
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-01-25 21:19:27 -0200

    app2 - ajustado a navegação das ofertas pela pesquisa

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
 
@@ -17,10 +17,12 @@ export class OfertaComponent implements OnInit, OnDestroy {
 	constructor(private route: ActivatedRoute, private ofertasServices: OfertasService) {}
 
 	ngOnInit() {
-		this.ofertasServices.getOfertaPotId(this.route.snapshot.params['id'])
-			.then(( oferta: Oferta) => {
-				this.oferta = oferta;
-            });
+        this.route.params.subscribe((parametros: Params) => {
+            this.ofertasServices.getOfertaPotId(parametros.id)
+                .then(( oferta: Oferta) => {
+                    this.oferta = oferta;
+                });
+        })
     }
     
     ngOnDestroy() {
--- app2/src/app/oferta/onde-fica/onde-fica.component.ts
+++ app2/src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from '../../ofertas.service';
 
 @Component({
     selector: 'app-onde-fica',
     templateUrl: './onde-fica.component.html',
-    styleUrls: ['./onde-fica.component.css']
+    styleUrls: ['./onde-fica.component.css'],
+    providers: [OfertasService]
 })
+
 export class OndeFicaComponent implements OnInit {
 
     private ondeFica = '';
@@ -14,10 +16,11 @@ export class OndeFicaComponent implements OnInit {
     constructor(private route: ActivatedRoute, private ofertasService: OfertasService) { }
 
     ngOnInit() {
-        this.ofertasService.getOndeFicaOfertaPorId(this.route.parent.snapshot.params['id'])
-            .then((resposta: string) => {
-                this.ondeFica = resposta;
-            });
-        
+        this.route.parent.params.subscribe((parametros: Params) => {
+            this.ofertasService.getOndeFicaOfertaPorId(parametros.id)
+                .then((resposta: string) => {
+                    this.ondeFica = resposta;
+                });
+        })
     }
 }
