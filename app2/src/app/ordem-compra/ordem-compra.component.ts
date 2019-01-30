04be8d97f81ad737144c83d74d8ad81ac5544c65
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-01-30 21:26:07 -0200

    app2 - aplicado validaçãoes basica de formulario e ajustando estilo nos imputs

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -13,6 +13,11 @@ export class OrdemCompraComponent implements OnInit {
     public complemento: string = '';
     public formaPagamento: string = '';
 
+    public enderecoValido: boolean;
+    public numeroValido: boolean;
+    public complementoValido: boolean;
+    public formaPagamentoValido: boolean; 
+
     constructor() { }
 
     ngOnInit() {
@@ -20,17 +25,41 @@ export class OrdemCompraComponent implements OnInit {
 
     public atualizaEndereco(endereco: string): void {
         this.endereco = endereco;
+
+        if (this.endereco.length > 3) {
+            this.enderecoValido = true;
+        } else {
+            this.enderecoValido = false;
+        }
     }
 
     public atualizaNumero(numero: string): void {
         this.numero = numero;
+
+        if (this.numero.length > 0) {
+            this.numeroValido = true;
+        } else {
+            this.numeroValido = false;
+        }
     }
 
     public atualizaComplemento(complemento: string): void {
         this.complemento = complemento;
+
+        if (this.complemento.length > 0) {
+            this.complementoValido = true;
+        } else {
+            this.complementoValido = false;
+        }
     }
 
     public atualizaFormaPagamento(formaPagamento: string): void {
         this.formaPagamento = formaPagamento;
+
+        if (this.formaPagamento.length > 0) {
+            this.formaPagamentoValido = true;
+        } else {
+            this.formaPagamentoValido = false;
+        }
     }
 }
