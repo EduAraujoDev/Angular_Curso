4b2f078a53ef126bd26579a3d8a4ceffeef54986
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-01-31 21:51:47 -0200

    app2 - Enviando o formulario para a API fake

--- app2/src/app/ordem-compra.service.ts
+++ app2/src/app/ordem-compra.service.ts
@@ -1,7 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+import { URL_API } from './app.api';
+import { map } from 'rxjs/operators';
+
 import { Pedido } from './shared/pedido.model';
 
+@Injectable()
 export class OrdemCompraService {
-    public efetivarCompra(pedido: Pedido): void {
-        console.log(pedido);
+
+    constructor(private http: Http) {  }
+
+    public efetivarCompra(pedido: Pedido): Observable<any> {
+        let headers: Headers = new Headers();
+
+        headers.append('Content-type', 'application/json');
+
+        return this.http.post(
+            `${URL_API}/pedidos`, 
+            JSON.stringify(pedido),
+            new RequestOptions({ headers: headers })
+        )
+        .pipe(map((resposta: Response) => console.log(resposta.json())))
     }
 }
