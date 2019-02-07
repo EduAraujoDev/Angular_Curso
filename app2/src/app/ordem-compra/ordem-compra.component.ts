7d01234c31d9635f9cfa76b53621daa9432a0000
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-02-07 20:07:03 -0200

    app2 - aplicado formGroup, ngSubmit e Validators para controlar e validar os dados do formulário

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { Pedido } from '../shared/pedido.model'
 
 @Component({
-  selector: 'app-ordem-compra',
-  templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+    selector: 'app-ordem-compra',
+    templateUrl: './ordem-compra.component.html',
+    styleUrls: ['./ordem-compra.component.css'],
+    providers: [OrdemCompraService]
 })
+
 export class OrdemCompraComponent implements OnInit {
 
-  constructor(private ordemCompraService: OrdemCompraService) { }
+    public formulario: FormGroup = new FormGroup({
+        'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(20) ]),
+        'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20) ]),
+        'complemento': new FormControl(null),
+        'formaPagamento': new FormControl(null, [ Validators.required ])
+    });
 
-  ngOnInit() {
-    
-  }
+    constructor(private ordemCompraService: OrdemCompraService) { }
 
-  public confirmarCompra(): void {
-  }
+    ngOnInit() {
+
+    }
+
+    public confirmarCompra(): void {
+        console.log(this.formulario);
+    }
 }
