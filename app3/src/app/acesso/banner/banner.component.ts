6241b21b07efda9e74ffd97b628762af0737e8b8
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-03-20 20:52:19 -0300

    app3 - trabalhando com triggers, state e style no animation

--- app3/src/app/acesso/banner/banner.component.ts
+++ app3/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state , style } from '@angular/animations';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.css'],
+    animations: [
+        trigger('banner', [
+            state('escondido', style({
+                opacity: 0
+            })),
+            state('visivel', style({
+                opacity: 1
+            }))
+        ])
+    ]
 })
+
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+    public estado: string = 'visivel';
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+
+    }
 }
