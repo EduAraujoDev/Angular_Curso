2e599a51eb4a0b5c30e3612853545f30ebe8e6bf
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-03-20 21:28:54 -0300

    app3 - Implementando a lógica da animação do banner

--- app3/src/app/acesso/banner/banner.component.ts
+++ app3/src/app/acesso/banner/banner.component.ts
@@ -25,7 +25,7 @@ export class BannerComponent implements OnInit {
     public estado: string = 'visivel';
 
     public imagens: Imagem[] = [
-        { estado: 'escondido', url: '/assets/banner-acesso/img_1.png' },
+        { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
         { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
         { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
         { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
@@ -35,10 +35,22 @@ export class BannerComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-
+        setTimeout(() => this.logicaRotacao(), 3000);
     }
 
-    public toogleEstado(): void {
-        this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+    public logicaRotacao(): void {
+        let idx: number;
+
+        for(let i:number = 0; i <= 4; i++) {
+            if (this.imagens[i].estado === 'visivel') {
+                this.imagens[i].estado = 'escondido';
+                idx = i === 4 ? 0 : i + 1;
+                break;
+            }
+        }
+
+        this.imagens[idx].estado = 'visivel';
+
+        setTimeout(() => this.logicaRotacao(), 3000);
     }
 }
