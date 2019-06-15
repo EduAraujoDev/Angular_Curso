182b739c5507d983ab15d43fb4ebb3f17183300b
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-06-15 19:59:51 -0300

    app3 - criado model usuário

--- app3/src/app/acesso/cadastro/cadastro.component.ts
+++ app3/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl } from "@angular/forms";
 
+import { Usuario } from "../usuario.model"
+
 @Component({
     selector: 'app-cadastro',
     templateUrl: './cadastro.component.html',
@@ -29,7 +31,16 @@ export class CadastroComponent implements OnInit {
         this.exibirPainel.emit('login');
     }
 
-    public cadastrarUsuario(): void{
+    public cadastrarUsuario(): void {
         console.log(this.formulario)
+
+        let usuario: Usuario = new Usuario (
+            this.formulario.value.email,
+            this.formulario.value.nome_completo,
+            this.formulario.value.nome_usuario,
+            this.formulario.value.senha
+        )
+
+        console.log(usuario)
     }
 }
---
+++ app3/src/app/acesso/usuario.model.ts
@@ -0,0 +1,8 @@
+export class Usuario {
+    constructor (
+        public email: string,
+        public norme_completo: string,
+        public nome_usuario: string,
+        public senha: string
+    ) { }
+}
