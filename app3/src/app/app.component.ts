7d1a802bcf64a2d3ea5650e7355e65a04a39ff70
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-06-15 22:06:18 -0300

    app3 - configurado conexão para o firebase

--- app3/src/app/app.component.ts
+++ app3/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as firebase from 'firebase'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app3';
+
+export class AppComponent implements OnInit {
+    title = 'app3';
+
+    ngOnInit(): void {
+        var firebaseConfig = {
+            apiKey: "AIzaSyBbgrBtxFYL5mt9fVzVw9e797hE0c4Wdg4",
+            authDomain: "jta-instagram-clone-78f66.firebaseapp.com",
+            databaseURL: "https://jta-instagram-clone-78f66.firebaseio.com",
+            projectId: "jta-instagram-clone-78f66",
+            storageBucket: "jta-instagram-clone-78f66.appspot.com",
+            messagingSenderId: "898277437159",
+            appId: "1:898277437159:web:6b4b653787290b22"
+          };
+
+        firebase.initializeApp(firebaseConfig)
+    }
 }
