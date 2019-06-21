bd4bf2be28b38bdb4e9a93748abeca9af0cc383e
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-06-20 22:24:39 -0300

    app2 - configurando as rotas

--- app3/src/app/app.module.ts
+++ app3/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@Angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+
+import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -12,6 +15,7 @@ import { CadastroComponent } from './Acesso/cadastro/cadastro.component';
 import { Autenticacao } from './autenticacao.service';
 import { HomeComponent } from './home/home.component';
 import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
+import { from } from 'rxjs';
 
 @NgModule({
     declarations: [
@@ -26,7 +30,8 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule.forRoot(ROUTES)
     ],
     providers: [ Autenticacao ],
     bootstrap: [AppComponent]
