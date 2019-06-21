827f2be297392ee1c2b108ab0324ade50fafb38e
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-06-21 20:35:22 -0300

    app3 - aplicando o botão logout

--- app3/src/app/home/home.component.ts
+++ app3/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { Autenticacao } from '../autenticacao.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
+
+@Injectable()
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private autenticacao: Autenticacao) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+    }
+
+    public sair(): void {
+        this.autenticacao.sair()
+    }
 }
