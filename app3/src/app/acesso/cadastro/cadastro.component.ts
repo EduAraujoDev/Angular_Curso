15035418f8b6fc8fb14f0763a30f678fdb4b28f6
EduAraujoDev <eduardo.araujo0@outlook.com>
2019-06-22 13:04:34 -0300

    app3 - solução atividades complementares 5

--- app3/src/app/acesso/cadastro/cadastro.component.ts
+++ app3/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 import { Usuario } from "../usuario.model"
 import { Autenticacao } from "../../autenticacao.service"
 
 @Component({
     selector: 'app-cadastro',
     templateUrl: './cadastro.component.html',
-    styleUrls: ['./cadastro.component.css']
+    styleUrls: ['./cadastro.component.css'],
+    animations: [
+        trigger('animacao-painel', [
+            state('criado', style({
+                opacity: 1
+            })),
+            transition('criado => alerta', [
+                animate('0.5s 0s ease-in-out', keyframes([
+                    style({ offset: 0.05, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.10, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.15, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.20, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.25, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.30, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.35, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.40, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.45, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.50, opacity: 1, transform: 'translateY(0)' })
+                ]))
+            ])
+        ])
+    ]
 })
 
 export class CadastroComponent implements OnInit {
@@ -16,6 +39,7 @@ export class CadastroComponent implements OnInit {
     public exibirPainel: EventEmitter<string> = new EventEmitter();
 
     public mensagemErro: string = ""
+    public estadoPainel: string = "criado"
 
     public formulario: FormGroup = new FormGroup({
         'email': new FormControl(null, [ Validators.required ]),
@@ -48,6 +72,11 @@ export class CadastroComponent implements OnInit {
             .then(() => this.exibirPainelLogin())
             .catch((error: Error) => {
                 this.mensagemErro = error.message
+                this.estadoPainel = "alerta"
             })
+    }
+
+    public fimDaAnimacao(): void {
+        this.estadoPainel = "criado"
     }
 }
--- app3/src/app/acesso/login/login.component.ts
+++ app3/src/app/acesso/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 import { Autenticacao } from '../../autenticacao.service'
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.css'],
+    animations: [
+        trigger('animacao-painel', [
+            state('criado', style({
+                opacity: 1
+            })),
+            transition('criado => alerta', [
+                animate('0.5s 0s ease-in-out', keyframes([
+                    style({ offset: 0.05, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.10, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.15, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.20, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.25, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.30, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.35, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.40, opacity: 1, transform: 'translateY(10px)' }),
+                    style({ offset: 0.45, opacity: 1, transform: 'translateY(-10px)' }),
+                    style({ offset: 0.50, opacity: 1, transform: 'translateY(0)' })
+                ]))
+            ])
+        ])
+    ]
 })
 
 export class LoginComponent implements OnInit {
@@ -15,6 +38,7 @@ export class LoginComponent implements OnInit {
     public exibirPainel: EventEmitter<string> = new EventEmitter<string>();
 
     public mensagemErro: string = ""
+    public estadoPainel: string = "criado"
 
     public formulario: FormGroup = new FormGroup({
         'email': new FormControl(null, [ Validators.required ] ),
@@ -36,6 +60,11 @@ export class LoginComponent implements OnInit {
         this.autenticacao.autenticar(this.formulario.value.email, this.formulario.value.senha)
             .catch((error: Error) => {
                 this.mensagemErro = error.message
+                this.estadoPainel = "alerta"
             })
+    }
+
+    public fimDaAnimacao(): void {
+        this.estadoPainel = "criado"
     }
 }
